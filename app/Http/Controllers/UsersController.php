bd8416b74e91db3eabfbb916259f52ca8d3db876
Evid Wong <76572677@qq.com>
2018-05-10 19:07:32 +0000

    sql

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Auth;
+use Illuminate\Support\Facades\DB;
 
 class UsersController extends Controller
 {
@@ -33,5 +34,26 @@ class UsersController extends Controller
 
         Auth::login($user);
         return redirect()->route('users.show', [$user])->with('success','欢迎，您将在这里开启一段新的旅程~');
+    }
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user,Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+         DB::enableQueryLog();
+        // var_dump($user);
+        $user->update([
+            'name' => $request->name,
+            'password' => bcrypt($request->password),
+        ]);
+        $sql=DB::getQueryLog();
+        var_dump($sql);
+        // return redirect()->route('users.show',$user->id)->with('success','更新成功');
     }
 }
